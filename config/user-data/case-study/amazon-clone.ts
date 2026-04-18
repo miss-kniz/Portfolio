07ba0a08c4925f6e48ac83fc8dbc5837158634a2
Miss kinz <mehak313naqvi@gmail.com>
2026-04-18 19:22:45 +0500

    Add amazon case study

---
+++ config/user-data/case-study/amazon-clone.ts
@@ -0,0 +1,12 @@
+import { CaseStudy } from "./cherished-lives";
+
+export const amazonCaseStudy: CaseStudy = {
+  bgImageUrl: "projects/case-study/amazon-clone.webp",
+
+  introduction:
+    "Amazon Clone is a frontend development project built to replicate a real-world e-commerce experience using only HTML, CSS, and JavaScript. It was created to strengthen my core web development fundamentals, especially DOM manipulation, UI structuring, and responsive design without any frameworks.",
+
+  overview:
+    "This project focuses on recreating a complex e-commerce interface inspired by Amazon using only core web technologies. It helped me practice real-world UI behavior, category filtering, local storage-based authentication, and responsive layout design while maintaining clean code structure.",
+  overviewImage: "/projects/amazon-clone.png",
+};
